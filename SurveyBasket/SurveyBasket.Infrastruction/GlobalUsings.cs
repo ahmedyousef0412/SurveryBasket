7f7efbf0223264bcd6997c37bc0d5ad63239da0f
ahmedyousef0412 <37086138+ahmedyousef0412@users.noreply.github.com>
2024-03-16 23:56:08 +0200

    1- Apply Clean  Architecture 2- Add CRUD operations to Polls Controller.

---
+++ SurveyBasket/SurveyBasket.Infrastruction/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using SurveyBasket.Application.Services;
+global using SurveyBasket.Infrastruction.Implementations;
+global using SurveyBasket.Domain.Entities;
+ 
